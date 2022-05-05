1162f40e95f22dd7a29379ac1e65fc7df1dc76d5
Karl Hajjar <karl.hajjar@polytechnique.edu>
2022-05-05 11:44:47 +0200

    Adding late notebook changes

--- learning-structure/networks/two_layer_net.py
+++ learning-structure/networks/two_layer_net.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 from pytorch_lightning import LightningModule
 
@@ -28,11 +29,23 @@ class TwoLayerNet(LightningModule):
         self.output_layer = nn.Linear(in_features=self.width, out_features=1, bias=self.bias)
         # self.output_layer = nn.Linear(in_features=self.width, out_features=1, bias=False)
 
-    def initialize_parameters(self):
-        input_weights = generate_uniform_sphere_weights(width=self.width, d=self.input_dim)
-        output_weights = generate_bernouilli_weights(width=self.width)
-        self.input_layer.weight.data.copy_(input_weights.data)
-        self.output_layer.weight.data.copy_(output_weights.data)
+    def initialize_parameters(self, mode='sphere_uniform'):
+        if mode == 'sphere_uniform':
+            input_weights = generate_uniform_sphere_weights(width=self.width, d=self.input_dim)
+            output_weights = generate_bernouilli_weights(width=self.width)
+            self.input_layer.weight.data.copy_(input_weights.data)
+            self.output_layer.weight.data.copy_(output_weights.data)
+        elif mode == 'ball_uniform':
+            input_weights = generate_uniform_sphere_weights(width=self.width, d=self.input_dim)
+            output_weights = generate_bernouilli_weights(width=self.width)
+            rs = torch.rand(size=(self.width, self.input_dim))
+            self.input_layer.weight.data.copy_(rs * input_weights.data)
+            self.output_layer.weight.data.copy_(output_weights.data)
+        elif mode == 'gaussian':
+            input_weights = torch.normal(mean=0, std=1, size=(self.width, self.input_dim))
+            output_weights = generate_bernouilli_weights(width=self.width)
+            self.input_layer.weight.data.copy_(input_weights.data)
+            self.output_layer.weight.data.copy_(output_weights.data)
 
     def forward(self, x):
         x = self.activation(self.input_layer.forward(x))
